1e1ea00250e503d94e488410453ab1ce7fc4fb50
YourName <your@email.com>
2026-05-18 14:33:51 +0530

    Using DATABASE_URL

--- server.js
+++ server.js
@@ -18,16 +18,7 @@ app.use(session({
 }));
 
 // MYSQL CONNECTION
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
 
