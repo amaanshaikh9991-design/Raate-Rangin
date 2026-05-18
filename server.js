dae2d77bd1ae77047d3a6a0bb9e538aecd3f0099
YourName <your@email.com>
2026-05-18 14:09:51 +0530

    Added DB port support

--- server.js
+++ server.js
@@ -22,12 +22,14 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
 
     if(err){
+        console.log("Database Connection Error:");
         console.log(err);
     } else {
         console.log("MySQL Connected");
